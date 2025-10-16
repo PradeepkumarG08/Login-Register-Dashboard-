782fe2cf1519f8443413cd9738cbf7918dafcf4c
PradeepkumarG08 <pradeepg2468@gmail.com>
2025-10-16 15:36:39 +0530

    Add vercel.json file

--- backend/index.js
+++ backend/index.js
@@ -10,14 +10,10 @@ console.log("MONGO_URI:", process.env.MONGO_URI);
 
 
 const app = express();
-app.use(cors({
-  origin: {"https://ai-dashboard-pi-umber.vercel.app"},
-  methods: {"POST","GET"},
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI || "mongodb+srv://pradeepg2468_db_user:pradeep2002@cluster1.meacgzu.mongodb.net/test", { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI , { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => console.log(" Connected to MongoDB Atlas"))
 .catch((err) => console.error(" MongoDB connection error:", err));
 
